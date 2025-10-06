4e1984caaafafb172789e2f0652c81572d95bc95
nhatngoonslaof <luunhat172@gmail.com>
2025-10-06 23:55:17 +0700

    improve category

--- controller/admin/product_category_controller.js
+++ controller/admin/product_category_controller.js
@@ -18,7 +18,7 @@ module.exports.index = async (req, res) => {
 
     let objectPagination = await paginationHelper({
         currentPage: 1,
-        limitItem: 4,
+        limitItem: 1000,
     }, req.query, countProduct);
 
     let sort = {};
@@ -28,28 +28,64 @@ module.exports.index = async (req, res) => {
     else {
         sort.position = "desc"
     }
+
+    function createTree(arr, parentId) {
+        const tree = [];
+        arr.forEach((item) => {
+            if (String(item.parent_id || "") === String(parentId || "")) {
+                const newItem = item.toObject ? item.toObject() : item;
+                const children = createTree(arr, item._id);
+                if (children.length > 0) {
+                    newItem.children = children;
+                }
+                tree.push(newItem);
+            }
+        });
+        return tree;
+    }
+
+    
+
     const records = await ProductCategory.find(find)
         .sort(sort)
         .limit(objectPagination.limitItem)
         .skip(objectPagination.skip)
-
+    const recordsTree = createTree(records, "");
     res.render("admin/pages/category/index", {
         pageTitle: "Danh mục sản phẩm",
-        records: records,
+        records: recordsTree,
         filterStatus,
         keyword,
         pagination: objectPagination
     });
 };
 
-module.exports.create = (req, res) => {
-    res.render("admin/pages/category/create", {
-        pageTitle: "Thêm mới sản phẩm"
-    });
-}   
+module.exports.create = async (req, res) => {
+    const records = await ProductCategory.find({ deleted: false }).sort({ position: 1 });
 
+    function createTree(arr, parentId) {
+        const tree = [];
+        arr.forEach((item) => {
+            if (String(item.parent_id || "") === String(parentId || "")) {
+                const newItem = item.toObject ? item.toObject() : item;
+                const children = createTree(arr, item._id);
+                if (children.length > 0) {
+                    newItem.children = children;
+                }
+                tree.push(newItem);
+            }
+        });
+        return tree;
+    }
+
+    const recordsTree = createTree(records, "");
+
+    res.render("admin/pages/category/create", {
+        pageTitle: "Thêm mới sản phẩm",
+        records: recordsTree, // 
+    });
+};
 module.exports.createPost = async (req, res,next) => {
-    console.log(req.body);
     if(req.body.position == "") {
         const countRecords = await ProductCategory.countDocuments();
         req.body.position = countRecords + 1;
